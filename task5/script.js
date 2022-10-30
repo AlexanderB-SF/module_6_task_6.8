ab5f17c8bb8b2a9154e3df578e38da8e37d9037d
AlexanderB-SF <henbae92@yandex.ru>
2022-10-30 18:22:32 +0300

    update

--- task5/script.js
+++ task5/script.js
@@ -2,6 +2,10 @@ let inputText = document.querySelector('input'),
     duplicateField = document.querySelector('#duplicateField');
 const button = document.querySelector('button');
 
+duplicateField.addEventListener('keydown', (e) => {
+    e.preventDefault();
+});
+
 inputText.addEventListener('keydown', (e) => {
     duplicateField.value += e.key;
 });
